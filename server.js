cd53d4f962af4318422aef08577e655d50e67331
TANG Thanh Lam <tanglam77@gmail.com>
2018-03-03 18:27:57 +0100

    update docker-compose :whale:

--- server.js
+++ server.js
@@ -9,19 +9,12 @@ const yaml = require('yamljs');
 //mongodb://mongo/tonguetwisterdb
 const dbURI = 'mongodb://mongo:27017/tonguetwister';
 //Set up default mongoose connection
+mongoose.connect(dbURI, function(err){
+  if(err){
+    console.log(err);
+  }
+});
 
-var connectWithRetry = function() {
-    return mongoose.connect(dbURI, function(err) {
-      if (err) {
-        console.error('Failed to connect to mongo on startup - retrying in 5 sec', err);
-        setTimeout(connectWithRetry, 10000);
-      }
-      else{
-        console.log('Successful connect to mongo!');
-      }
-    });
-};
-connectWithRetry();
 //swagger
 const swaggerUi = require('swagger-ui-express');
 const swaggerdoc = yaml.load('./swagger/swagger.yaml');
